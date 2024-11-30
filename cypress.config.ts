c6e5169e5424df67344ad545ccb76ccc1cb0b23b
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-11-30 20:27:58 +0200

    build: 🔧 configured `cypress`

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      bundler: "webpack",
+      framework: "next",
+    },
+  },
+  e2e: {
+    setupNodeEvents(on, config) {},
+  },
+});
