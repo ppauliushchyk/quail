19416f7086534a3a044c746b5436b6e7b0226197
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-11-30 13:10:55 +0200

    feat: 🆕 added bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import React from "react";
+
+import "@/styles/index.scss";
 
 export const metadata: Metadata = {
   description: "",
@@ -12,8 +15,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html data-bs-theme="light" lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </head>
+
+      <body>
+        {children}
+
+        <Script src="/scripts/popper.min.js" />
+        <Script src="/scripts/bootstrap.min.js" />
+      </body>
     </html>
   );
 }
