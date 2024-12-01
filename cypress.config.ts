ec22728cf1c5c73eec36862235689d3876a2d004
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-12-01 03:25:19 +0200

    build: 🔧 updated config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import { addMatchImageSnapshotPlugin } from "@simonsmith/cypress-image-snapshot/plugin";
 
 export default defineConfig({
   component: {
@@ -6,8 +7,13 @@ export default defineConfig({
       bundler: "webpack",
       framework: "next",
     },
+    setupNodeEvents(on) {
+      addMatchImageSnapshotPlugin(on);
+    },
   },
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on) {
+      addMatchImageSnapshotPlugin(on);
+    },
   },
 });
