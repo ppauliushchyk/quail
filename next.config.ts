e37ee824aefdb369ea41c46593261ef705cf8a9f
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-12-01 13:27:25 +0200

    build: 📃 updated logging config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+      hmrRefreshes: true,
+    },
+  },
+};
 
 export default nextConfig;
